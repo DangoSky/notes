370214c33c099e91e805788ee5fbfd954e00c9a1
dangosky <1341514988@qq.com>
2019-10-21 18:32:51 +0800

    update notes
    Change-Id: I839406453043eff13272984be8500810fb18ce63

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,28 @@ module.exports = {
   description: '笔记记了是要常常翻出来看的，不然干嘛要花那么多时间记笔记？',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
-  ]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'JavaScript', link: '/' },
+      { text: 'React', link: '/guide/' },
+      { text: 'TypeScript', link: '/typescript' }
+    ],
+    sidebarDepth: 2,
+    sidebar: [
+      // {
+      //   title: 'JavaScript',
+      //   children: [
+      //     '/javascript.md',
+      //   ]
+      // },
+      {
+        title: 'React',
+        children: [
+          '/React/state.md',
+          '/React/生命周期.md',
+        ]
+      }
+    ]
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -2,4 +2,4 @@
 home: true
 actionText: 温故而知新 →
 heroImage: /home.jpg
-actionLink: /zh/guide/
+actionLink: /home/
